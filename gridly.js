f622fd1da22f6e146d1db9f110fc549e5ba076cd
Samori Gorse <samorigorse@gmail.com>
2013-06-25 23:07:51 +0200

    Made a proper example

--- gridly.js
+++ gridly.js
@@ -1,35 +1,37 @@
 ;(function(window, $) {
 
-  $.fn.gridly = function gridly(options) {
-    var divs = $(this).find(' > div');
-    divs.css('position', 'absolute');
+  $.fn.gridly = function gridly(options, selector) {
+    options = options || {};
+    selector = selector || ' > div';
 
-    var cols = options.cols;
-    var rows = Math.ceil(divs.length / cols);
+    var els = $(this).find(selector).css('position', 'absolute')
+      , cols = options.cols || 5
+      , rows = Math.ceil(els.length / cols);
 
-    var sample = divs.slice(0, 1);
-    var sampleHeight = sample.outerHeight();
-    var sampleWidth = sample.outerWidth();
+    var gutter = options.gutter || 20
+      , sample = els.slice(0, 1)
+      , sampleHeight = sample.outerHeight()
+      , sampleWidth = sample.outerWidth();
 
     function position() {
-      divs.each(function() {
+      els.each(function() {
         var $this = $(this)
           , row = $this.data('row')
           , col = $this.data('col');
 
         $this
           .animate({
-            'top': sampleHeight * col + col * options.gutter,
-            'left': sampleWidth * row + row * options.gutter
+            'top': sampleHeight * col + col * gutter,
+            'left': sampleWidth * row + row * gutter
           })
       });
     }
 
-    function calculate() {
+    function grid() {
       var c = 0;
       for(var i = 0; i < rows; i++ ) {
         for(var j = 0; j < cols; j ++) {
-          var el = divs[c];
+          var el = els[c];
           if(!el) break;
           $(el)
             .data('row', j)
@@ -39,54 +41,18 @@
       }
     }
 
-    function paint() {
-      divs.each(function() {
-        var rgb = Math.floor(Math.random() * 2);
-        var color = Math.ceil(Math.random() * 255);
-        var s = [];
-        s[rgb] = color;
-
-        for(var i = 0; i < 3; i++) {
-          if(!s[i]) {
-            s[i] = Math.ceil(Math.random() * 255);
-          }
-        }
-
-        $(this).css('background', 'rgb( ' + s.join(',') + ' )');
-      });
+    function draw() {
+      grid();
+      position();
     }
 
-    $('#sort-by-name').click(function() {
-      Array.prototype.sort.call(divs, function(a, b) {
-        var aName = $(a).data('name');
-        var bName = $(b).data('name');
+    return {
 
-        if(aName > bName) return 1;
-        if(aName == bName) return 0;
-        if(aName < bName) return -1;
-      });
+      els: els,
 
-      calculate();
-      position();
-    });
+      draw: draw
 
-    $('#sort-by-id').click(function() {
-      Array.prototype.sort.call(divs, function(a, b) {
-        var aName = $(a).data('id');
-        var bName = $(b).data('id');
-
-        if(aName > bName) return 1;
-        if(aName == bName) return 0;
-        if(aName < bName) return -1;
-      });
-
-      calculate();
-      position();
-    });
-
-    calculate();
-    position();
-    paint();
+    }
   };
 
 }(this, jQuery));
